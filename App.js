66747be2f1b52b747c012935d803a39b0328675c
Khanh Nguyen <khanh@Khanhs-MacBook-Pro.local>
2023-07-17 13:45:44 -0700

    changes

--- App.js
+++ App.js
@@ -10,6 +10,29 @@ const CHATBOT_USER_OBJ = {
 export default function App() {
   const [messages, setMessages] = useState([]);
 
+  const questions = [
+    {
+      question1: "What's the first name of Snap's CEO?",
+      answer: "Evan",
+      correct: "You are correct",
+      incorrect: "Nope!",
+    },
+    {
+      question1: "What 1 + 1?",
+      answer: "2",
+      correct: "You are correct",
+      incorrect: "Nope!",
+    }
+    ,
+    {
+      question1: "What's 2 + 2?",
+      answer: "4",
+      correct: "You are correct",
+      incorrect: "Nope!",
+    }
+    
+  ]
+
   useEffect(() => {
     setMessages([
       {
@@ -40,17 +63,60 @@ export default function App() {
     ]);
   };
 
+  var index = 0;
+  var gameStarted = false;
+  const [output, setOutput] = useState("Ok let's play! Press any character to start")
+  var questionPrompted = false
+
+
   const respondToUser = (userMessages) => {
     console.log("User message text:", userMessages[0].text);
 
     // Simple chatbot logic (aka Checkpoint 2 onwards) here!
 
-    addBotMessage("I am da response!");
+    if(gameStarted == false)
+    {
+      if(userMessages[0].text == "Yes") {
+        addBotMessage(output)
+        gameStarted = true
+      }
+      else {
+        addBotMessage("Please say 'yes' to start.")
+      }
+    }
+    else if(gameStarted == true)
+    { 
+      askQuestions(userMessages)
+    }
   };
+
+  const askQuestions = (userMessages) => {
+    if(questionPrompted == false)
+    {
+      addBotMessage(questions[index].question1)
+      questionPrompted = true
+    }
+    else if(userMessages[0].text == questions[index].answer && index < 3)
+    {
+      addBotMessage("Correct! Next Question! Say ok to start")
+      index = index + 1
+      questionPrompted = false
+    }
+    else
+    {
+      addBotMessage("Wrong try again!")
+    }
+
+    if(index == 3)
+    {
+      addBotMessage("You win!")
+    }
+  }
+
 
   const onSend = useCallback((messages = []) => {
     addNewMessage(messages);
-    // setTimeout(() => respondToUser(messages), 1000);
+    setTimeout(() => respondToUser(messages), 1000);
   }, []);
 
   return (
